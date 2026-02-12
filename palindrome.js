1486d1f2b4fd87828ee639863883be69346ad7b2
nkh97430@gmail.com <nkh97430@gmail.com>
2026-02-12 19:13:37 +0100

    exercice Palindrome suite

--- palindrome.js
+++ palindrome.js
@@ -1,24 +1,40 @@
-const isPalindrome = (stringChar) => {
-    let chaineInverse = stringChar.split('').reverse().join('');
-    if (stringChar === chaineInverse) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// const isPalindrome = (stringChar) => {
+//     let chaineInverse = stringChar.split('').reverse().join('');
+//     if (stringChar === chaineInverse) {
+//         return "true";
+//     } else {
+//         return "false";
+//     }
+// }
 
 const isPalindromeWithoutReverseAndJoin = (stringChar) => {
-    // @todo pour la semaine prochaine.
-}
+    // je convertis ma chaine de caractère en tableau
+    let firstString = stringChar.split('');
+    let reverseString = "";
+    // je fais une boucle inversée sur le tab (For)
+    // console.log(firstString);
+    for (let i = firstString.length - 1 ; i>=0; i--) {
+           reverseString += firstString[i]; 
+         }
+         console.log(reverseString);
+    // je convertit le tableau en string
+    if (stringChar === reverseString) {
+        return "True";
+    } else {
+        return "False";
+    }
+    //je compare le résultat avec stringChar
+    //je mets des If et return false et True
+};
 
-console.log(isPalindrome("anna")); // Retourne true
-console.log(isPalindrome("nasser")); // Retourne false
-console.log(isPalindrome("kayak")); // Retourne true
-console.log(isPalindrome("arbre")); // Retourne false
+// console.log(isPalindrome("anna")); // Retourne true
+// console.log(isPalindrome("nasser")); // Retourne false
+// console.log(isPalindrome("kayak")); // Retourne true
+// console.log(isPalindrome("arbre")); // Retourne false
 
 
 
-console.log(isPalindromeWithoutReverseAndJoin("anna")); // Retourne true
+console.log(isPalindromeWithoutReverseAndJoin("zorro")); // Retourne true
 console.log(isPalindromeWithoutReverseAndJoin("nasser")); // Retourne false
 console.log(isPalindromeWithoutReverseAndJoin("kayak")); // Retourne true
 console.log(isPalindromeWithoutReverseAndJoin("arbre")); // Retourne false
